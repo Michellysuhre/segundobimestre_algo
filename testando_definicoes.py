09e3ab8ef67882d1d04ff3ce4bc85008be88c8f6
Michelly Aparecida Suhre <michellysuhre@gmail.com>
2025-05-26 15:25:19 -0400

    Add files via upload

---
+++ testando_definicoes.py
@@ -0,0 +1,47 @@
+# compras = ["pão", "café", "leite","queijo"]
+# print(compras[3])
+
+# #compras.remove("café")
+# #print(compras)
+# #pode se removido pelo nome ou pelo indice
+
+# compras.remove(compras[1])
+# print(compras)
+
+# compras.append("presunto")
+# compras.append("requeijão")
+# compras.append("bolo de chocolate")
+# print(compras)
+# #no append só pode adicionar um item por vez
+
+# compras.sort
+# print(compras)
+
+# compras_ordenada=sorted(compras)
+# print(compras_ordenada)
+# #é preciso criar uma lista nova para receber os elementos ordenadas da lista antiga
+
+# for item in compras:
+#     print("-", item)
+# #idependente do nome após o for, vai rodar do mesmo jeito
+
+
+# #while opcao =="nao":
+#     ##compras.remove(compras[])
+#     #print(compras)
+
+#  #item_remover = input("Digite o nome do item que deseja remover: ")
+#         #if item_remover in compras:
+#             #compras.remove(item_remover)
+#             #
+
+# lista = []
+# dicionarios = { chave: valor}
+# tuplas = ()
+# conjuntos = set()
+
+teste=(1,2,3,4)
+print(teste)
+teste.remove(1)
+print(teste)
+
